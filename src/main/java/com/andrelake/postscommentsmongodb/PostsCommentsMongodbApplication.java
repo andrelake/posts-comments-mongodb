8f3e7d2e7bc0aafcdf590e64fffc26bb6d6cba9c
andrelake <andre.lake@hotmail.com>
2020-04-15 16:59:05 -0300

    Project created

---
+++ src/main/java/com/andrelake/postscommentsmongodb/PostsCommentsMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.andrelake.postscommentsmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostsCommentsMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostsCommentsMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrelake/postscommentsmongodb/PostsCommentsMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrelake.postscommentsmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsCommentsMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
