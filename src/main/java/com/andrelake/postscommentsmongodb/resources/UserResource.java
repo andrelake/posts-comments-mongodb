7bfd60f2269513c36b08ec262a0350562369b676
andrelake <andre.lake@hotmail.com>
2020-04-18 12:03:48 -0300

    DTO pattern to return Users

--- src/main/java/com/andrelake/postscommentsmongodb/resources/UserResource.java
+++ src/main/java/com/andrelake/postscommentsmongodb/resources/UserResource.java
@@ -1,13 +1,16 @@
 package com.andrelake.postscommentsmongodb.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.andrelake.postscommentsmongodb.domain.User;
+import com.andrelake.postscommentsmongodb.dto.UserDTO;
 import com.andrelake.postscommentsmongodb.services.UserService;
 
 @RestController
@@ -18,8 +21,11 @@ public class UserResource {
 	private UserService userService;
 	
 	@GetMapping
-	public List<User> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		
-		return userService.findAll();
+		List<User> list = userService.findAll();
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());  //stream p/ list poder receber lambda
+																									//map funcionando como um foreach
+		return ResponseEntity.ok(listDto);															//Collectors.toList para voltar a ser list
 	}
 }
