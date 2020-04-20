c01c7c3a1e9c567d4237a00a5742698be5d4b7c0
andrelake <andre.lake@hotmail.com>
2020-04-20 15:16:55 -0300

    AuthorDTO

--- src/main/java/com/andrelake/postscommentsmongodb/config/Instantiation.java
+++ src/main/java/com/andrelake/postscommentsmongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.andrelake.postscommentsmongodb.domain.Post;
 import com.andrelake.postscommentsmongodb.domain.User;
+import com.andrelake.postscommentsmongodb.dto.AuthorDTO;
 import com.andrelake.postscommentsmongodb.repository.PostRepository;
 import com.andrelake.postscommentsmongodb.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para SP, abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Bom dia aeeee, galere!", maria);
-
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para SP, abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Bom dia aeeee, galere!", new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
