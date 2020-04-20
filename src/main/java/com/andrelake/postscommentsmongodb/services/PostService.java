cc390f69646dfbf2da30c4c3cbf6d6a1f7745589
andrelake <andre.lake@hotmail.com>
2020-04-20 16:47:19 -0300

    FullSearch

--- src/main/java/com/andrelake/postscommentsmongodb/services/PostService.java
+++ src/main/java/com/andrelake/postscommentsmongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.andrelake.postscommentsmongodb.services;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,5 +27,11 @@ public class PostService {
 	public List<Post> findByTitle(String text) {
 		
 		return postRepository.findByTitleContainingIgnoreCase(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		
+		maxDate = new Date(maxDate.getTime() + 24*60*60*1000);
+		return postRepository.fullSearch(text, minDate, maxDate);
 	}
 }
