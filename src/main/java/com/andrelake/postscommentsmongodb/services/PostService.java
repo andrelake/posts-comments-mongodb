7c44fac8e6509bd8b425d9e0128f6a0cbd04ad40
andrelake <andre.lake@hotmail.com>
2020-04-20 16:06:27 -0300

    Search with query method

--- src/main/java/com/andrelake/postscommentsmongodb/services/PostService.java
+++ src/main/java/com/andrelake/postscommentsmongodb/services/PostService.java
@@ -1,5 +1,7 @@
 package com.andrelake.postscommentsmongodb.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,5 +21,10 @@ public class PostService {
 				.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 
 		return post;
+	}
+	
+	public List<Post> findByTitle(String text) {
+		
+		return postRepository.findByTitleContainingIgnoreCase(text);
 	}
 }
