c7740834f3775c0ff4004d19107b43be1b43f31c
andrelake <andre.lake@hotmail.com>
2020-04-20 15:46:36 -0300

    Add comments in posts

--- src/main/java/com/andrelake/postscommentsmongodb/config/Instantiation.java
+++ src/main/java/com/andrelake/postscommentsmongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.andrelake.postscommentsmongodb.domain.Post;
 import com.andrelake.postscommentsmongodb.domain.User;
 import com.andrelake.postscommentsmongodb.dto.AuthorDTO;
+import com.andrelake.postscommentsmongodb.dto.CommentDTO;
 import com.andrelake.postscommentsmongodb.repository.PostRepository;
 import com.andrelake.postscommentsmongodb.repository.UserRepository;
 
@@ -41,10 +42,16 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para SP, abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Bom dia aeeee, galere!", new AuthorDTO(maria));
 
+		CommentDTO com1 = new CommentDTO("Boa viagem, mano", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO com2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO com3 = new CommentDTO("Uhul!!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(com1, com2));
+		post2.getComments().addAll(Arrays.asList(com3));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
 		userRepository.save(maria);
 	}
-
 }
